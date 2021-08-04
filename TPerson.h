1eb83309bc0739592301c9e2767c8ff0e60c4161
Hojagulyyev Rovshen <aleksrow@yandex.ru>
2021-08-04 12:19:19 +0500

    04.08.2021 part 4_2

---
+++ TPerson.h
@@ -0,0 +1,51 @@
+
+#pragma once
+#include <vcl.h>
+#include "enumeration.h"
+
+class TPerson
+{
+  public:
+		  TPerson();
+		 ~TPerson();
+
+		 void   set_First_name(const AnsiString& set);
+		 void   set_Last_name(const AnsiString& set);
+		 void   set_Year_of_birth(TDate set);
+		 void   set_Gender_of_person(gender& set);
+		 void   set_Place_of_birth(const AnsiString& set = "No");
+		 void   set_Country(const AnsiString& set = "Turkmenistan");
+		 void   set_City(const AnsiString& set = "Ashgabat");
+		 void	set_Andress_registration(const AnsiString& set);
+		 void   set_Place_of_resindece(const AnsiString& set);
+		 void   set_Home_telephone(const AnsiString& set);
+		 void   set_Mobile_telephone(const AnsiString& set);
+		 void   set_Mail(const AnsiString& set);
+
+		 AnsiString   get_First_name();
+		 AnsiString   get_Last_name();
+		 TDate        get_Year_of_birth();
+		 gender       get_Gender_of_person();
+		 AnsiString   get_Place_of_birth();
+		 AnsiString   get_Country();
+		 AnsiString   get_City();
+		 AnsiString	  get_Andress_registration();
+		 AnsiString   get_Place_of_resindece();
+		 AnsiString   get_Home_telephone();
+		 AnsiString   get_Mobile_telephone();
+		 AnsiString   get_Mail();
+
+  protected:
+		 AnsiString            First_name;
+		 AnsiString            Last_name;
+		 TDate                 Year_of_birth;
+		 gender                Gender_of_person;
+		 AnsiString            Place_of_birth;
+		 AnsiString            Country;
+		 AnsiString            City;
+		 AnsiString			   Andress_registration;
+		 AnsiString            Place_of_resindece;
+		 AnsiString            Home_telephone;
+		 AnsiString            Mobile_telephone;
+		 AnsiString            Mail;
+};
---
+++ enumeration.h
@@ -0,0 +1,3 @@
+#pragma once
+
+enum gender {Man, Woman};
